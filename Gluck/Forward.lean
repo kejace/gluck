5a2867b49143b86c34d8c8ffa227d6b7e9a12069
kejace <kejace@gmail.com>
2026-07-17 00:15:33 -0400

    feat(Forward): refine endpoint circle mesh

--- Gluck/Forward.lean
+++ Gluck/Forward.lean
@@ -10,6 +10,7 @@ import Gluck.Forward.Section4CircleSpliceRadius
 import Gluck.Forward.Section4CircleSpliceNonconcyclic
 import Gluck.Forward.Section4CircleSpliceAssembly
 import Gluck.Forward.Section4OddMeshDensity
+import Gluck.Forward.Section4EndpointMeshDensity
 import Gluck.Forward.Section4CircleArc
 import Gluck.Forward.Section4CircleSpliceSimple
 import Gluck.Forward.Section4EndpointCurvature
